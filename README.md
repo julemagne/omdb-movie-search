2176a5cdc4d82d68c6e5bfb1d77e0ab59c07bd78
Julie David <julie.angela.david@gmail.com>
2017-04-09 19:34:20 -0400

    Add basic routing to a movie search component

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MovieSearch
+# Movie Search with OMDb
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 1.0.0.
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,31 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+        RouterModule.forRoot([
+      {
+        path: 'search',
+        component: MoviesComponent
+      },
+      {
+        path: '',
+        redirectTo: '/search',
+        pathMatch: 'full'
+      },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
