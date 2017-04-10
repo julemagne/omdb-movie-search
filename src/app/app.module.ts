eee74db4720d85ca3aa72a8db12494c2b4e84133
Julie David <julie.angela.david@gmail.com>
2017-04-09 21:16:05 -0400

    Extract routing to app-routing module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule }   from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { MoviesComponent } from './movies/movies.component';
+
+import { AppRoutingModule }     from './app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,17 +17,7 @@ import { MoviesComponent } from './movies/movies.component';
     BrowserModule,
     FormsModule,
     HttpModule,
-        RouterModule.forRoot([
-      {
-        path: 'search',
-        component: MoviesComponent
-      },
-      {
-        path: '',
-        redirectTo: '/search',
-        pathMatch: 'full'
-      },
-    ])
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
